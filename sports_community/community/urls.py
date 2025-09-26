0bdd38bfb6625efea0c66ab4062fef6cfa2aafa1
Anyaibe <chimeremezeanyaibe73@gmail.com>
2025-09-26 10:57:29 +0100

    Addition of new URLs

--- sports_community/community/urls.py
+++ sports_community/community/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'community'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('create/', views.create_post, name='create_post'),
 ]
